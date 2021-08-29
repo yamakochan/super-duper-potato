b91cad3939e47c853853cf99794446a71d7189dc
yamakochan <tom.yum.yum.k@gmail.com>
2021-08-29 22:20:52 +0900

    sp ver2

--- public/js/card-base.js
+++ public/js/card-base.js
@@ -672,6 +672,7 @@ class Background extends createjs.Container{
 				canvasElement.setAttribute("width" ,cns_stageWidth);
 				canvasElement.setAttribute("height" ,cns_stageHeight);
 				stage.scaleX = stage.scaleY = 1 / cns_scale;
+				background.scaleX = background.scaleY = 1;
 			}
 		}
     }
