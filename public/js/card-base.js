87f698d3f65d745c3861e8512ff69895475125e1
yamakochan <tom.yum.yum.k@gmail.com>
2021-08-29 14:57:30 +0900

    sp ver1

--- public/js/card-base.js
+++ public/js/card-base.js
@@ -591,7 +591,7 @@ class Background extends createjs.Container{
         this.on("dblclick", this.handledblclick,this);
 
         // すまほ用
-        this.addEventListener("touchstart", this.handleTS);
+        this.addEventListener('touchstart', this.handleTS);
         this.on("touchmove", this.handleTM,this);
         this.on("touchend", this.handleTE,this);
     }
