68044ee81844ebb18c6602773523ecb56ad91063
yamakochan <tom.yum.yum.k@gmail.com>
2021-09-05 22:44:31 +0900

    sp ver1

--- public/js/card-base.js
+++ public/js/card-base.js
@@ -648,11 +648,11 @@ class Background extends createjs.Container{
 		        // 	if(layer1.x < cns_stageWidth / cns_scale - cns_layer1SideMargin)
 
 
-	        	if(layer1.x * -1 < (cns_layer1Left - cns_layer1SideMargin) / cns_scale){
-	        		layer1.x = (cns_layer1Left - cns_layer1SideMargin) / cns_scale * -1;
+	        	if(layer1.x * -1 < (cns_layer1Left - cns_layer1SideMargin) * cns_scale){
+	        		layer1.x = (cns_layer1Left - cns_layer1SideMargin) * cns_scale * -1;
 	        	}else{
-		        	if(layer1.x  * -1 > (cns_layer1Right - cns_stageWidth + cns_layer1SideMargin) / cns_scale){
-		        		layer1.x = (cns_layer1Right - cns_stageWidth + cns_layer1SideMargin) / cns_scale * -1;
+		        	if(layer1.x  * -1 > (cns_layer1Right - cns_stageWidth + cns_layer1SideMargin) * cns_scale){
+		        		layer1.x = (cns_layer1Right - cns_stageWidth + cns_layer1SideMargin) * cns_scale * -1;
 		        	}else{
 						this.prex2 = this.prex;
 						this.prex = layer1.x;
