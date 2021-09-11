1515a3fd1aa01fa88aabd97171fec81e31b372c5
yamakochan <tom.yum.yum.k@gmail.com>
2021-09-11 16:09:39 +0900

    sp ver1

--- public/js/card-base.js
+++ public/js/card-base.js
@@ -654,32 +654,21 @@ class Background extends createjs.Container{
 	        	}
 
 			}else{
+				// 前回スケールを保管
 		        this.preScale = this.scale;
 
+				// backgroundのスケールを変更
 		        this.p1 = event.nativeEvent.targetTouches[0];
 		        this.p2 = event.nativeEvent.targetTouches[1];
 		        this.pinchDist2 = Math.abs(this.p1.pageX - this.p2.pageX) + Math.abs(this.p1.pageY - this.p2.pageY);
 		        this.scale = this.pinchDist2 / this.pinchDist;
 
-		        this.adjustx = (this.pinchCenterx - layer1.x) * this.scale - (this.pinchCenterx - layer1.x) * this.preScale;
-		        this.adjusty = (this.pinchCentery - layer1.y) * this.scale - (this.pinchCentery - layer1.y) * this.preScale;
+		        // 拡大縮小の中心を調整
+		        this.adjustx = (this.pinchCenterx - layer1.x) / this.scale - (this.pinchCenterx - layer1.x) / this.preScale;
+		        this.adjusty = (this.pinchCentery - layer1.y) / this.scale - (this.pinchCentery - layer1.y) / this.preScale;
 
-		// let notice11 = new Notice(-50,0,this.pinchCenterx,"GhostWhite",20,1);
-		// let notice12 = new Notice(-50,50,layer1.x,"GhostWhite",20,1);
-
-		// let notice15 = new Notice(-50,100,this.adjustx,"GhostWhite",20,1);
-		// let notice14 = new Notice(-50,150,this.adjusty,"GhostWhite",20,1);
-
-		// let notice13 = new Notice(-50,200,layer1.x,"GhostWhite",20,1);
-		// let notice16 = new Notice(-50,250,layer1.y,"GhostWhite",20,1);
-
-				layer1.x = layer1.x - this.adjustx;
-				layer1.y = layer1.y - this.adjusty;
-
-				// cns_scale = this.scale;
-				// stage.scaleX = stage.scaleY = 1 / cns_scale;
-
-// ここで直接処理してもいいが、stageの属性変更であるため、一応global functionにする。
+				layer1.x = layer1.x + this.adjustx;
+				layer1.y = layer1.y + this.adjusty;
 			}
 		}
     }
