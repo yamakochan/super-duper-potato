0a98eb65d28b6216eb2ee90b61121a1bdf6e7860
yamakochan <tom.yum.yum.k@gmail.com>
2021-09-18 17:19:28 +0900

    sp ver2

--- public/js/card-base.js
+++ public/js/card-base.js
@@ -104,7 +104,7 @@ class Judge{
 
 		// デッキ表示
 		this.deck = new Deck();
-		background.addChild(this.deck);
+		layer1.addChild(this.deck);
 	    // deckにカードを追加   
 		for (let i = 0; i < argDeckList.length; i++){
 			let xcard = new Card(cns_passPrefix + argDeckList[i],i);
@@ -116,7 +116,7 @@ class Judge{
 
 		// cemetary表示
 		this.cemetary = new Cemetary();
-		background.addChild(this.cemetary);
+		layer1.addChild(this.cemetary);
 	    // cemetaryにカードを追加    
 	    let j = argDeckList.length;
 		for (let i = 0; i < argCemetaryList.length; i++){
@@ -663,7 +663,7 @@ class Background extends createjs.Container{
 		        this.pinchDist2 = Math.abs(this.p1.pageX - this.p2.pageX) + Math.abs(this.p1.pageY - this.p2.pageY);
 		        this.scale = this.pinchDist2 / this.pinchDist * this.backupScale;
 
-		        // 拡大縮小の中心を調整 
+		        // 拡大縮小の中心を調整 （scaleを大きくしたらadjustはプラス　ー＞　pinchCenterとlayer1の中心の距離を離す。）
 		        this.adjustx = (this.pinchCenterx - layer1.x) - (this.pinchCenterx - layer1.x) * this.preScale / this.scale;
 		        this.adjusty = (this.pinchCentery - layer1.y) - (this.pinchCentery - layer1.y) * this.preScale / this.scale;
 
